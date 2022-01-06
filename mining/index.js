db86b0217fd899edbed2766901fac1bc00b6b4fb
jsg0629 <tnrms513@gmail.com>
2022-01-06 17:31:05 +0900

    32

--- mining/index.js
+++ mining/index.js
@@ -7,7 +7,7 @@
 '1211d11341113411';
 'aaawwdqwfewfewddwqdwqsaawaaasadwadaa';
 'fewdfefdfsfdwfdeffewfewfdqdf';
-'fewdddfefdsdfddwafddeffdeddwfsdewdqdf';
+'fewdddfefdsdfddwafdddeffdeddwfsdewdqdf';
 =======
 
 @@@@채굴 대 성공@@@@

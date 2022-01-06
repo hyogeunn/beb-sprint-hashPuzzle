a9ccc91b09ced7cacf852caa513a7e0682bc652c
jsg0629 <tnrms513@gmail.com>
2022-01-06 17:32:09 +0900

    39

--- mining/index.js
+++ mining/index.js
@@ -7,7 +7,7 @@
 '1211d11341113411';
 'aaawwdqwfewfewddwaqdwqsaawaaasadwadaa';
 'fewdfefdfsfdwfdeffewfewfdqdff';
-'fewdddfefdsddfddwafdddeddffdeddwfsfdewdqdf';
+'fewdddfefdsddfddwafzdddeddffdeddwfsfdewdqdf';
 =======
 
 @@@@채굴 대 성공@@@@

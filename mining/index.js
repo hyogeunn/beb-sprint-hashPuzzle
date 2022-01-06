cabb300a3224e83b1e660b840e3bcb5ec4eda135
jsg0629 <tnrms513@gmail.com>
2022-01-06 17:30:23 +0900

    27

--- mining/index.js
+++ mining/index.js
@@ -7,7 +7,7 @@
 '1211d11341113411';
 'aaawwdqwfewfewddwqdwqsaaaaasadwadaa';
 'fewdfefdfsfdwfdeffewfewfdqdf';
-'fewdddfefdsdfddwafdeffewfsewdqdf';
+'fewdddfefdsdfddwafddeffewfsewdqdf';
 =======
 
 @@@@채굴 대 성공@@@@
